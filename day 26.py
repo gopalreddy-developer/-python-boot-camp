13f6344cae428b0e8b8d53352e1b34548dc5f93e
gopalreddy-developer <85780327+gopalreddy-developer@users.noreply.github.com>
2021-07-14 19:50:25 +0530

    day 26

---
+++ day 26.py
@@ -0,0 +1,23 @@
+
+name = ["HP","DELL","LENOVO","APPLE","ASUS"]
+for x in enumerate(name,10):
+    print(x)
+    
+
+
+
+for count , x in enumerate(name,16):
+	y = count,x
+	print(list(y))
+
+	
+name =("HP","DELL","LENOVO","APPLE","ASUS")
+for x in enumerate (name,101):
+    print(x)
+
+	
+for count , x in enumerate(name,200):
+	y = count,x
+	print(y)
+
+	
